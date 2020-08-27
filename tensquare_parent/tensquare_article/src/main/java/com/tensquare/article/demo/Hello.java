28e7c22067f93e0834435fc310223729fb093315
liuawen <157514367@qq.com>
2020-08-27 20:15:54 +0800

    项目搭建

---
+++ tensquare_parent/tensquare_article/src/main/java/com/tensquare/article/demo/Hello.java
@@ -0,0 +1,11 @@
+package com.tensquare.article.demo;
+
+/**
+ * @author Liu Awen Email:willowawen@gmail.com
+ * @create 2020-08-27
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println(Integer.valueOf(1));
+    }
+}
